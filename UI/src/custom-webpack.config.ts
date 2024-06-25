dc6e800fac09144563be52a9c70f85eb1dfabe2a
Craig McLellen <craig.mclellen@insight.com>
2024-06-25 14:39:40 +0800

    Fix

--- UI/src/custom-webpack.config.ts
+++ UI/src/custom-webpack.config.ts
@@ -1,5 +1,13 @@
 import { EnvironmentPlugin } from 'webpack';
 const Dotenv = require('dotenv-webpack');
+
+var dotenvPath = `./.env`;
+var nodeEnv = process.env['NODE_ENV'];
+if(!!nodeEnv) {
+  dotenvPath += `.${process.env['NODE_ENV']}`;
+}
+
+console.log(dotenvPath);
 module.exports = {
-  plugins: [new Dotenv()],
+  plugins: [new Dotenv({ path: dotenvPath })],
 };
